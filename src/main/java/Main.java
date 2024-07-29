e1f7a360ca6c5a9e010fe57e8f8bf3a0dfdb3709
W A T Amasha Fernando <143680645+gitFerdo@users.noreply.github.com>
2024-07-29 10:18:28 +0000

    Integer
    DataTypes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 class Main {
-  static int age;
-  
   public static void main(String[] args) {
-    int age = 27;
+    // interger
+    byte aSingleByte = 100; // -128 to 127
+    short aSmallNumber = 20000; // -32,768 to 32,767
+    int anInteger = 2147483647; // -2147483648 to 2147483647
+    long aLargeNumber = 9223372036854775807L; // -9223372036854775808 to 9223372036854775807
+
     
-    System.out.println("I am " + age + " years old.");
   }
 }
