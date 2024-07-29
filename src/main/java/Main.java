12b769968ffb64a8d23499cb634def1d0653c554
W A T Amasha Fernando <143680645+gitFerdo@users.noreply.github.com>
2024-07-29 09:58:43 +0000

    set up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,11 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
-public class Main {
+class Main {
   public static void main(String[] args) {
     System.out.println("Hello world");
   }
+}
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+class AnotherClass {
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
 }
